6465ea731a5ca46248a0adf09c5959e57ece72f2
zxxz <viacheslav.modestov@gmail.com>
2018-10-18 00:02:26 +0300

    sync find

--- search-stdin/main.go
+++ search-stdin/main.go
@@ -5,28 +5,55 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"strings"
 )
 
 // usage echo -e 'first\nsecond\nthird\n' | go run main.go
+func countGo(s string) int {
+	resp, err := http.Get(s)
+	if err != nil {
+		log.Printf("%v", err)
+		return -1
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("%v", err)
+		return -2
+	}
+	str := string(body[:])
+	idx := strings.Count(str, "Go")
+	if idx > 0 {
+		return idx
+	} else {
+		return -3
+	}
+}
+
 func main() {
+	var total int
 	input := os.Stdin
 	buf := bufio.NewReader(input)
-	idx := 0
 	for {
 		line, err := buf.ReadBytes('\n')
 		if err != nil {
 			if err == io.EOF {
-				os.Exit(0)
+				break
 			}
 			log.Fatal(err)
 		}
 
-		line = bytes.TrimSpace(line)
 		if len(line) > 0 {
-			idx = idx + 1
-			fmt.Printf("Line #%d: %s\n", idx, line)
+			line = bytes.TrimSpace(line)
+			url := string(line[:])
+			res := countGo(url)
+			total = total + res
+			fmt.Printf("Go string #%d: %s\n", res, line)
 		}
 	}
+	fmt.Printf("Total: %d: \n", total)
 }
