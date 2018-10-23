02dce4211ba1cdd9d2ba0b7a973db0cf73c81b4a
zxxz <viacheslav.modestov@gmail.com>
2018-10-23 10:04:52 +0300

    walk stdin: minor improvements, tries, not working, before completion of te!

--- search-stdin/main.go
+++ search-stdin/main.go
@@ -1,40 +1,51 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
+	// "bufio"
+	// "bytes"
+	"flag"
 	"fmt"
-	"io"
+	// "io"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"strings"
+	// "os"
+	"regexp"
 )
 
 // usage echo -e 'first\nsecond\nthird\n' | go run main.go
-func countGo(s string) chan int {
+func countGo(url string, re *regexp.Regexp) chan int {
 	ch := make(chan int, 1)
 	go func() {
-		defer close(ch)
-		resp, err := http.Get(s)
+		resp, err := http.Get(url)
+		defer func() {
+			close(ch)
+			resp.Body.Close()
+		}()
 		if err != nil {
 			log.Printf("%v", err)
 			ch <- -1
+			return
 		}
-		defer resp.Body.Close()
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			log.Printf("%v", err)
 			ch <- -2
+			return
 		}
-		str := string(body[:])
-		idx := strings.Count(str, "Go")
+		ma := re.FindAll(body, -1)
+		if ma == nil {
+			ch <- -3
+			return
+		}
+		idx := len(ma)
 		if idx > 0 {
 			ch <- idx
-			fmt.Printf("Count for %s: %d\n", s, idx)
+			fmt.Printf("Count for %s: %d\n", url, idx)
+			return
 		} else {
-			ch <- -3
+			ch <- -4
+			return
 		}
 	}()
 	return ch
@@ -50,27 +61,46 @@ func multyplexor(common, input chan int) {
 
 func main() {
 	var total int
-	var urls []string
-	input := os.Stdin
-	buf := bufio.NewReader(input)
-	for {
-		line, err := buf.ReadBytes('\n')
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			log.Fatal(err)
-		}
-
-		if len(line) > 0 {
-			line = bytes.TrimSpace(line)
-			url := string(line[:])
-			urls = append(urls, url)
-		}
+	// var urls []string
+	var search_str string
+	flag.StringVar(&search_str, "e", "Go", "regular expression to search in location")
+	args := flag.Args()
+	flag.Parse()
+	fmt.Printf("%s\n", search_str)
+	// input := ourl.Stdin
+	// buf := bufio.NewReader(input)
+	// for {
+	// 	line, err := buf.ReadBytes('\n')
+	// 	if err != nil {
+	// 		if err == io.EOF {
+	// 			break
+	// 		}
+	// 		log.Fatal(err)
+	// 	}
+	//
+	// 		if len(line) > 0 {
+	// 			line = bytes.TrimSpace(line)
+	// 			url := string(line[:])
+	// 			urls = append(urls, url)
+	// 		}
+	// 	}
+	if args == nil || len(args) == 0 {
+		fmt.Println("Must provide at least one url for search")
+		return
 	}
-	c := make(chan int, int(len(urls)/2))
-	for _, url_string := range urls {
-		multyplexor(c, countGo(url_string))
+	cb := int(len(args) / 2)
+	if cb <= 0 {
+		cb = 1
+	}
+	c := make(chan int, cb)
+	defer close(c)
+	re, err := regexp.Compile(search_str)
+	if err != nil {
+		fmt.Println("Can not compile provided regexp")
+		return
+	}
+	for _, url_string := range args {
+		multyplexor(c, countGo(url_string, re))
 	}
 	counter := 0
 	for res := range c {
@@ -78,8 +108,8 @@ func main() {
 		if res > 0 {
 			total = total + res
 		}
-		if counter == len(urls) {
-			close(c)
+		if counter == len(args) {
+			break
 		}
 	}
 	fmt.Printf("Total: %d: \n", total)
--- walk_tree/main.go
+++ walk_tree/main.go
@@ -1,49 +1,46 @@
 package main
+
 import (
-"golang.org/x/tour/tree"
-"fmt"
-"time"
+	"fmt"
+	"golang.org/x/tour/tree"
+	"time"
 )
+
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
-func Walk(t *tree.Tree, ch chan int){
-ch <- t.Value
-if tl := t.Left; tl != nil{
-Walk(tl, ch)
-}
-if tr := t.Left; tr != nil{
-Walk(tr, ch)
-}
+func Walk(t *tree.Tree, ch chan int) {
+	ch <- t.Value
+	if tl := t.Left; tl != nil {
+		Walk(tl, ch)
+	}
+	if tr := t.Left; tr != nil {
+		Walk(tr, ch)
+	}
 }
 
-
-func fibonacci(n int, ch chan int){
+func fibonacci(n int, ch chan int) {
+	defer close(ch)
 	x, y := 0, 1
 	for i := 0; i < n; i++ {
 		ch <- x
-time.Sleep(time.Second)
+		time.Sleep(time.Second)
 		x, y = y, x+y
 	}
-	close(ch)
 }
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 // func Same(t1, t2 *tree.Tree) bool
 func main() {
-// t := tree.New(1)
-ch := make(chan int)
-// Walk(t, ch)
-go fibonacci(10, ch)
-for i := range ch{
-fmt.Printf("% d", i)
-}
-fmt.Printf("\n")
-/*
-a := [] int{1, 2, 3}
-b := [] int{1, 2, 3}
-if a == b {
-fmt.Println("same")
-}
-*/
+	t := tree.New(1)
+	fmt.Println(t)
+	ch := make(chan int)
+	Walk(t, ch)
+	//	go fibonacci(10, ch)
+	go func() {
+		for i := range ch {
+			fmt.Printf("% d", i)
+		}
+	}()
+	fmt.Printf("\n")
 }
